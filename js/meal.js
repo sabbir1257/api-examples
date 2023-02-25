99fcb954e9a8df608e562491a83951e3466c43ae
sabbir1257 <masabbir1257@gmail.com>
2023-02-26 04:28:01 +0600

    push

--- js/meal.js
+++ js/meal.js
@@ -1,49 +1,3 @@
-// const loadMeal = (searchText) => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => displayMeal(data.meals));
-// };
-
-// const displayMeal = (meals) => {
-//   // console.log(Meals)
-//   // step 1 : container element
-//   const mealContainer = document.getElementById("meals-container");
-//   // mealContainer.innerHTML = '' ;
-//   meals.forEach(meal => {
-//     console.log(meal)
-//     // step 2: create child for each element
-//     const mealDiv = document.createElement("div");
-//     mealDiv.classList.add("col");
-//     // step 3: set content of the child
-//     mealDiv.innerHTML = `
-//         <div class="card">
-//                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-//                 <div class="card-body">
-//                   <h5 class="card-title">${meal.strMeal}</h5>
-//                   <p class="card-text">This is a longer card with supporting text below as a natural 
-//                     lead-in to additional content. This content is a little bit longer.</p>
-//                 </div>
-//               </div>
-//         `;
-        
-//         // step 4: appendChaild
-//         mealContainer.appendChild(mealDiv)
-        
-//     });
-//     };
-    
-// const searchMeals = () => {
-//     const searchText = document.getElementById('search-field').value;
-//     // search meals
-//     console.log(searchText);
-//     loadMeal(searchMeals);
-// }
-
-// loadMeal('fish');
-
-
-
 const loadMeals = (searchText) =>{
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
   console.log(url)
@@ -68,8 +22,10 @@ const displayMeals = meals =>{
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <button onclick="loadMealDetail2(${meal.idMeal})"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
+               This content is a little bit longer.</p>
+              <button onclick="loadMealDetail2(${meal.idMeal})"  type="button" class="btn btn-primary" data-bs-toggle="modal"
+               data-bs-target="#mealDetails">
                   Details
               </button>
           </div>
@@ -87,7 +43,6 @@ const searchMeals = () =>{
   // search meals
   console.log(searchText);
   loadMeals(searchText);
-  
 }
 
 const loadMealDetail = idMeal => {
